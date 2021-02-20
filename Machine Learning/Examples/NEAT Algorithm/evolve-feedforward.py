bdd421cfa3a0ea2cdd3a12fb88808f66701da797
Tom McLean <64887735+mcleantom@users.noreply.github.com>
2021-02-20 14:00:59 +0000

    More machine learning

---
+++ Machine Learning/Examples/NEAT Algorithm/evolve-feedforward.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 13:50:02 2021
+
+@author: mclea
+"""
+
+import multiprocessing
+import os
+import pickle
+
+import neat
+
+import cart_pole
+import gym
+import numpy as np
+
+
+runs_per_net = 2
+
+
+# Use the NN network phenotype and the discrete actuator force function.
+def eval_genome(genome, config):
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+
+    fitnesses = []
+
+    for runs in range(runs_per_net):
+        env = gym.make("CartPole-v0")
+        observation = env.reset()
+        # Run the given simulation for up to num_steps time steps.
+        fitness = 0.0
+        done = False
+        while not done:
+            action = net.activate(observation)
+            observation, reward, done, info = env.step(action)
+            fitness += reward
+
+        fitnesses.append(fitness)
+
+    # The genome's fitness is its worst performance across all runs.
+    return np.mean(fitnesses)
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = eval_genome(genome, config)
+
+
+def run():
+    # Load the config file, which is assumed to live in
+    # the same directory as this script.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    pop = neat.Population(config)
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.StdOutReporter(True))
+
+    pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
+    winner = pop.run(pe.evaluate)
+
+    # Save the winner.
+    with open('winner-feedforward', 'wb') as f:
+        pickle.dump(winner, f)
+
+    print(winner)
+
+
+if __name__ == '__main__':
+    run()
---
+++ Machine Learning/Examples/NEAT Algorithm/thisisneat.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 13:41:51 2021
+
+@author: mclea
+"""
+
+import gym
+
+env = gym.make("CartPole-v0")
+observation = env.reset()
+
+print(observation)
+print(env.action_space)
+
+done = False
+while not done:
+    observation, reward, done, info = env.step(env.action_space.sample())
+    print(env.action_space.sample())
+    env.render()
+
+env.close()
